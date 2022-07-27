4f68d3070bb93d9f899cc190ceb0168c14fef554
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-07-27 11:14:01 -0300

    feat: adicionado componente header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,10 @@
 import styles from './header.module.css';
+import igniteLogo from '../assets/ignite-logo.svg';
 
 export default function(){
   return (
     <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
       <strong>Ignite Feed</strong>
     </header>
   )
