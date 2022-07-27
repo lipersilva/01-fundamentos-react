0884eb0c9b109c04f9601c1d09a8c2de012a23e5
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-07-27 10:59:17 -0300

    Initial commit :tada:

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+import styles from './header.module.css';
+
+export default function(){
+  return (
+    <header className={styles.header}>
+      <strong>Ignite Feed</strong>
+    </header>
+  )
+}
