41ac9b3a006489fce43140c7d6d1a086239cada9
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-07-27 15:02:48 -0300

    feat: adicionado componente de 'Sidebar'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import Header from './components/header'
+import Header from './components/Header'
 import './global.css'
+import styles from './App.module.css'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <>
-      <Header/>  
+      <Header/> 
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          feed
+        </main>  
+      </div> 
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react'
+
+export default function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80" 
+        alt="" 
+      />
+      <div className={styles.profile}>
+        <img 
+          className={styles.avatar} 
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" 
+          alt="" />
+        <strong>Filipe Rodrigues</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
