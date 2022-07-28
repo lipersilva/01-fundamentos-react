5cb5b20ae447e98e171a8d7a019313fc33e009a3
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-07-28 10:34:29 -0300

    feat: adicionado propriedades do componente 'Post'

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,43 @@ import styles from './App.module.css'
 import Sidebar from './components/Sidebar'
 import { Post } from './components/Post'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars0.githubusercontent.com/u/8186664?v=4',
+      name: 'Filipe Rodrigues',
+      role: 'Front-end Developer'
+    },
+    content: [
+      { type: 'pharagraph', content:'Fala galeraa 👋'},
+      { type: 'pharagraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare' } 
+        // <a href=''> #novoprojeto</a>{' '}
+        // <a href=''> #nlw </a> {' '}
+        // <a href=''> #rocketseat </a>
+    ],
+    publishedAt: new Date('2022-07-28 09:48:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=50&q=80',
+      name: 'Rebeca Rodrigues',
+      role: 'Back-end Developer'
+    },
+    content: [
+      { type: 'pharagraph', content:'Fala galeraa 👋'},
+      { type: 'pharagraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare' } 
+        // <a href=''> #novoprojeto</a>{' '}
+        // <a href=''> #nlw </a> {' '}
+        // <a href=''> #rocketseat </a>
+    ],
+    publishedAt: new Date('2022-07-28 10:00:00'),
+  }
+]
+
 function App() {
   return (
     <>
@@ -11,8 +48,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post/>
-          <Post/>
+          {posts.map(post => {
+            return (
+              <Post
+                author= {post.author}
+                content= {post.content}
+                publishedAt= {post.publishedAt}
+              />
+            )
+          })}
         </main>  
       </div> 
     </>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,39 +1,58 @@
 import styles from './Post.module.css';
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-export function Post(){
+const comments = [
+  1,
+  2,
+  3,
+]
+
+export function Post({ author, publishedAt, content }){
+
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'",{
+    locale: ptBR,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
+
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
           <Avatar
-            src= "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=50&q=80"
+            src={author.avatarUrl}
           />
           <div className={styles.authorInfo}>
-            <strong>Jane Cooper</strong>
-            <span>Dev Front-End</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
         <time 
-          dateTime='2022-05-11 08:00:00'
-          title='11 de Maio às 08:00'
+          dateTime={publishedAt.toISOString()}
+          title={publishedDateFormatted}
         >
-          Publicado há 1h
+          {publishedDateRelativeToNow} 
         </time>
       </header>
 
       <div className={styles.content}>
+        {content.map(line => {
+          if (line.type === 'pharagraph') {
+            return <p>{line.content}</p>
+          } else if (line.type === 'link') {
+            return <p><a href={line.content}>{line.content}</a></p>
+          }
+        })}
         
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p><a href=''> jane.design/doctorcare </a> </p>
-        <p> 
-          <a href=''> #novoprojeto</a>{' '}
-          <a href=''> #nlw </a> {' '}
-          <a href=''> #rocketseat </a>
-        </p>
       </div>
 
       <form className={styles.commentForm}>
@@ -44,7 +63,9 @@ export function Post(){
         </footer>
       </form>
       <div className={styles.commentList}>
-        <Comment/>
+        {comments.map(comments => {
+          return <Comment/>
+        })}
       </div>
     </article>
   );
